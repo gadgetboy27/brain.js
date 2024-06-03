f1d8986f742d270c0e067a334eee5e0ca4839bcf
Henry-Peti <iamgadgetboy@gmail.com>
2024-06-04 05:35:38 +1200

    camera changes

--- index.js
+++ index.js
@@ -1,128 +1,69 @@
+require('dotenv').config();
+// getting places from APIs
+function loadPlaces(position) {
+    const params = {
+        radius: 300,    // search places not farther than this value (in meters)
+        clientId: process.env.FOURSQUARE_CLIENT_ID,
+        clientSecret: process.env.FOURSQUARE_CLIENT_SECRET,
+        version: '20300101',    // foursquare versioning, required but unuseful for this demo
+    };
+    console.log(clientId)
+    // CORS Proxy to avoid CORS problems
+    const corsProxy = 'https://cors-anywhere.herokuapp.com/';
+
+    // Foursquare API (limit param: number of maximum places to fetch)
+    const endpoint = `${corsProxy}https://api.foursquare.com/v2/venues/search?intent=checkin
+        &ll=${position.latitude},${position.longitude}
+        &radius=${params.radius}
+        &client_id=${params.clientId}
+        &client_secret=${params.clientSecret}
+        &limit=30 
+        &v=${params.version}`;
+    return fetch(endpoint)
+        .then((res) => {
+            return res.json()
+                .then((resp) => {
+                    return resp.response.venues;
+                })
+        })
+        .catch((err) => {
+            console.error('Error with places API', err);
+        })
+};
+
+
 window.onload = () => {
-  let method = 'dynamic';
+    const scene = document.querySelector('a-scene');
 
-  // if you want to statically add places, de-comment following line
-  method = 'static';
+    // first get current user location
+    return navigator.geolocation.getCurrentPosition(function (position) {
 
-  if (method === 'static') {
-      let places = staticLoadPlaces();
-      renderPlaces(places);
-  }
+        // than use it to load from remote APIs some places nearby
+        loadPlaces(position.coords)
+            .then((places) => {
+                places.forEach((place) => {
+                    const latitude = place.location.lat;
+                    const longitude = place.location.lng;
 
-  if (method !== 'static') {
+                    // add place name
+                    const placeText = document.createElement('a-link');
+                    placeText.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+                    placeText.setAttribute('title', place.name);
+                    placeText.setAttribute('scale', '15 15 15');
+                    
+                    placeText.addEventListener('loaded', () => {
+                        window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+                    });
 
-      // first get current user location
-      return navigator.geolocation.getCurrentPosition(function (position) {
-
-          // than use it to load from remote APIs some places nearby
-          dynamicLoadPlaces(position.coords)
-              .then((places) => {
-                  renderPlaces(places);
-              })
-      },
-          (err) => console.error('Error in retrieving position', err),
-          {
-              enableHighAccuracy: true,
-              maximumAge: 0,
-              timeout: 27000,
-          }
-      );
-  }
+                    scene.appendChild(placeText);
+                });
+            })
+    },
+        (err) => console.error('Error in retrieving position', err),
+        {
+            enableHighAccuracy: true,
+            maximumAge: 0,
+            timeout: 27000,
+        }
+    );
 };
-
-function staticLoadPlaces() {
-  return [
-      {
-          name: "My place name",
-          location: {
-              lat: 0, // add here latitude if using static data
-              lng: 0, // add here longitude if using static data
-          }
-      },
-      {
-          name: 'Another place name',
-          location: {
-              lat: 44.498529, 
-              lng: 11.356657,
-          }
-      }
-  ];
-}
-
-// getting places from REST APIs
-async function dynamicLoadPlaces(position) {
-  let params = {
-      radius: 300,    // search places not farther than this value (in meters)
-      clientId: 'UK32CEVITYO5AMHU3ZRAASDZ25QCODXPSJ2P0LW3ANSJ55E5',   // add your credentials here
-      clientSecret: 'TZY0JD4AY2QZFNK124NEW2DGMRFVH34EHJ1CF1A42FTFIGHG',   // add your credentials here
-      version: '20300101',    // foursquare versioning, required but unuseful for this demo
-  };
-
-  // CORS Proxy to avoid CORS problems
-  let corsProxy = 'https://cors-anywhere.herokuapp.com/';
-
-  // Foursquare API
-  let endpoint = `${corsProxy}https://api.foursquare.com/v2/venues/search?intent=checkin
-      &ll=${position.latitude},${position.longitude}
-      &radius=${params.radius}
-      &client_id=${params.clientId}
-      &client_secret=${params.clientSecret}
-      &limit=15
-      &v=${params.version}`;
-  return fetch(endpoint)
-      .then((res) => {
-          return res.json()
-              .then((resp) => {
-                  return resp.response.venues;
-              })
-      })
-      .catch((err) => {
-          console.error('Error with places API', err);
-      })
-};
-
-function renderPlaces(places) {
-  let scene = document.querySelector('a-scene');
-
-  places.forEach((place) => {
-      const latitude = place.location.lat;
-      const longitude = place.location.lng;
-
-      // add place icon
-      const icon = document.createElement('a-image');
-      icon.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
-      icon.setAttribute('name', place.name);
-      icon.setAttribute('src', '../assets/map-marker.png');
-
-      // for debug purposes, just show in a bigger scale, otherwise I have to personally go on places...
-      icon.setAttribute('scale', '20, 20');
-
-      icon.addEventListener('loaded', () => window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')));
-
-      const clickListener = function (ev) {
-          ev.stopPropagation();
-          ev.preventDefault();
-
-          const name = ev.target.getAttribute('name');
-
-          const el = ev.detail.intersection && ev.detail.intersection.object.el;
-
-          if (el && el === ev.target) {
-              const label = document.createElement('span');
-              const container = document.createElement('div');
-              container.setAttribute('id', 'place-label');
-              label.innerText = name;
-              container.appendChild(label);
-              document.body.appendChild(container);
-
-              setTimeout(() => {
-                  container.parentElement.removeChild(container);
-              }, 1500);
-          }
-      };
-
-      icon.addEventListener('click', clickListener);
-
-      scene.appendChild(icon);
-  });
-}
