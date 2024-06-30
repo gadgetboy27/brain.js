1075c34101f8bc0c58cd951dc2208769ee2a637c
Henry-Peti <iamgadgetboy@gmail.com>
2024-07-01 04:27:11 +1200

    changes to add arrow glb

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function loadPlaces(position) {
         radius: 300,    // search places not farther than this value (in meters)
         clientId: 'UK32CEVITYO5AMHU3ZRAASDZ25QCODXPSJ2P0LW3ANSJ55E5',
         clientSecret: 'TZY0JD4AY2QZFNK124NEW2DGMRFVH34EHJ1CF1A42FTFIGHG',
-        version: '20240628',    // foursquare versioning, required but unuseful for this demo
+        version: '20240128',  // foursquare versioning, required but unuseful for this demo
     };
 
     // CORS Proxy to avoid CORS problems
